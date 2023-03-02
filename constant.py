ce2e32984ffa043a7422f5fc980edf8c4225d3cf
Trinh Huy <giahuy050201@gmail.com>
2023-03-02 11:13:25 +0200

    Only benchmark lightweight model

--- constant.py
+++ constant.py
@@ -13,27 +13,26 @@ imageClassModelName = [
 ]
 
 objectDetectModelName = [
-    "fasterrcnn_resnet50_fpn_v2",
     "fasterrcnn_mobilenet_v3_large_320_fpn",
     "fasterrcnn_mobilenet_v3_large_fpn",
-    "fasterrcnn_resnet50_fpn",
-    "fcos_resnet50_fpn",
-    "retinanet_resnet50_fpn_v2",
-    "retinanet_resnet50_fpn",
     "ssd300_vgg16",
     "ssdlite320_mobilenet_v3_large",
-    quit,
+    # "fasterrcnn_resnet50_fpn_v2", 
+    # "fasterrcnn_resnet50_fpn",
+    # "fcos_resnet50_fpn",
+    # "retinanet_resnet50_fpn_v2",
+    # "retinanet_resnet50_fpn",
 ]
 
 modelLinkDrive = {
     "alexnet": "https://drive.google.com/u/2/uc?id=1tv9NPj9RaYAsek8hCnsFx_YPdfyt9c6t",
-    "fasterrcnn_resnet50_fpn_v2": "https://drive.google.com/u/2/uc?id=1DAqbZmcp8a8WJbWhPZmBBhHFkBGtsVMh",
+    # "fasterrcnn_resnet50_fpn_v2": "https://drive.google.com/u/2/uc?id=1DAqbZmcp8a8WJbWhPZmBBhHFkBGtsVMh",
     "fasterrcnn_mobilenet_v3_large_320_fpn": "https://drive.google.com/u/2/uc?id=18m3-apGnDGwE_X5_AuojFhiYbeJU8mlq",
     "fasterrcnn_mobilenet_v3_large_fpn": "https://drive.google.com/u/2/uc?id=1vtdn-fpadNaBBscOxa1vCrhAhX1ADwEK",
-    "fasterrcnn_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1gSyeMNMyXSooTnTFR8meNhR3N-F1HUkb",
-    "fcos_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1IzsgAfMs4uxib42BFv9S8ybWP2jcTdMr",
-    "retinanet_resnet50_fpn_v2": "https://drive.google.com/u/2/uc?id=1Zv3wTj8EbVMUGqRpjfNPOmRNAPXaIhdv",
-    "retinanet_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1NEDQCGFQO5z2CmNbyNBx_wVSBjSM1LuO",
+    # "fasterrcnn_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1gSyeMNMyXSooTnTFR8meNhR3N-F1HUkb",
+    # "fcos_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1IzsgAfMs4uxib42BFv9S8ybWP2jcTdMr",
+    # "retinanet_resnet50_fpn_v2": "https://drive.google.com/u/2/uc?id=1Zv3wTj8EbVMUGqRpjfNPOmRNAPXaIhdv",
+    # "retinanet_resnet50_fpn": "https://drive.google.com/u/2/uc?id=1NEDQCGFQO5z2CmNbyNBx_wVSBjSM1LuO",
     "ssd300_vgg16": "https://drive.google.com/u/2/uc?id=1eoC_3tH_s0HbuMTJ-j2zEBr1uTTFe529",
     "ssdlite320_mobilenet_v3_large": "https://drive.google.com/u/2/uc?id=14qxtYiZ6euKSYRgePFK4XI9UQAaopvCO",
 }
